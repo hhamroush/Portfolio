94bcd0da5d1085976470856fad06aef08b346146
hhamroush <63880437+hhamroush@users.noreply.github.com>
2023-01-19 18:08:48 -0400

    NavBar v0.1.0

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -9,14 +9,16 @@
     
 </script>
 
-<nav>
-    <div class="content-center">
-        <ul>
-            {#each navItems as items}
-            <li>
-                <a href={items.href}>{items.label}</a>
-            </li> 
-            {/each}
-        </ul>
-    </div>
-</nav>
+<div class="pointer-events-auto hidden md:block">
+    <nav class="w-fit mx-auto my-4 rounded-full flex items-center justify-center flex-row">
+        <div>
+            <ul class="flex rounded-full bg-white/90 px-4 text-sm font-medium dark:bg-zinc-800/90 dark:text-zinc-200">
+                {#each navItems as items}
+                    <li class="relative block px-3 py-2">
+                        <a href={items.href}>{items.label}</a>
+                    </li> 
+                {/each}
+            </ul>
+        </div>
+    </nav>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,12 @@
 </script>
 
 <div>
-	<nav />
+	<Nav />
 
 	<main>
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	
 </div>
 
